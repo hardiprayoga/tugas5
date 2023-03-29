e6c213a0071bb330a7e310290adddd51592f22e0
hardiprayoga <hardiprayoga007@gmail.com>
2023-03-29 11:15:51 +0700

    tugas5

---
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/kgK8Nc8k/1c.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'TAHARA KOI',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
+
+class NewsDetailScreenHot extends StatefulWidget {
+  const NewsDetailScreenHot({
+    super.key, 
+    required this.newsDetailHot,
+  });
+  final String newsDetailHot;
+
+  @override
+  State<NewsDetailScreenHot> createState() => _NewsDetailScreenHotState();
+}
+
+class _NewsDetailScreenHotState extends State<NewsDetailScreenHot> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/cCfhKSsb/2c.jpg",
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  children: [
+                   const SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment:MainAxisAlignment.center ,
+                      children: const [
+                        Text(
+                          'TAHARA KOI',
+                           style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+          ],
+          
+        ),
+      ),
+    );
+  }
+}
